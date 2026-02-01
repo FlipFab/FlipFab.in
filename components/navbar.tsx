54dd8ca47d1ead399d2ec57b5b36d1945a19ab25
FlipFab <letsmailoffice@gmail.com>
2026-02-01 22:57:46 +0530

    Delete components/navbar.tsx

--- components/navbar.tsx
+++
@@ -1,7 +0,0 @@
-export default function Navbar() {
-  return (
-    <nav className="p-4 border-b border-gray-800 flex justify-between">
-      <div className="gold text-xl">FlipFab</div>
-    </nav>
-  )
-}
